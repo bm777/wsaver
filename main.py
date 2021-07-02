b7a8c27c160d9416670281a620e5596fa0341fcc
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-02 10:24:13 +0100

    addded font google font Barlow& Comfortaa

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import os
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtGui import QFontDatabase
+from PySide2.QtCore import QDir
 
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
 
     engine = QQmlApplicationEngine()
-    font = QFontDatabase.addApplicationFont(":/font/Barlow.ttf")
+    font_path = os.path.join(CURRENT_DIRECTORY, "font", "Barlow/Barlow-Regular.ttf")
+
+    _id = QFontDatabase.addApplicationFont(font_path)
+    print(QFontDatabase.applicationFontFamilies(_id))
     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
 
     if not engine.rootObjects():
--- qml/side/Content.qml
+++ qml/side/Content.qml
@@ -4,22 +4,17 @@ Item {
     property int temperature: 12
     property string unit: "C"
     property string day: "Monday, 16:00"
-//    FontLoader {id: comfortaa; source: "../../font/Comfortaa.ttf"}
-    FontLoader {id: barlow; source: "../../font/Barlow.ttf"}
+
     property alias nameFontfamily: name.font.family
 
     Text {
         id: name
         x: parent.width * 0.1
         y: x
-        text: "12"
-        font.family: "Courier"
+        text: "Stack overflow"
+        font.family: "Comfortaa"
         font { pointSize: 30; italic: false}
     }
 }
 
-/*##^##
-Designer {
-    D{i:0;autoSize:true;height:480;width:640}
-}
-##^##*/
+
