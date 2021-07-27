c5b773e6c0b3d9d13002a00d24ac498ee5aed631
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-27 16:28:14 +0100

    Update on wsaver/ Adding and enabling search optio on left Side

--- git-sync.sh
+++ git-sync.sh
@@ -4,7 +4,7 @@ while true
 do
 	echo "_______________"$i"_________________"
 	git add -A
-	git commit -am "Update on wsaver/ Adding and enabling search optio on left Side.n"
+	git commit -am "Update on wsaver/ Adding and enabling search optio on left Side"
 	git push
 	sleep 30
 	echo "Hi, bm7. A update has been done :)"
