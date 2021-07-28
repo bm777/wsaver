48b3822047cbcd8fd11a5d51b2302ecaccfa3d69
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-28 01:53:41 +0100

    Update on wsaver/ Adding and enabling search optio on left Side

--- local_request.py
+++ local_request.py
@@ -2,6 +2,9 @@ import json
 
 def import_forecast(filename):
     # Function to read data from local json file on the hard drive
+    # Args:
+    # - filename : the file name of the json which contains forecasts data
+    # - the function return data var
     with open("filename") as f:
         data = json.load(f)
     return data
