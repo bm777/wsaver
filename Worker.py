f9ba796a2942c266780dc8ef1d34dac470758b4c
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-28 02:08:02 +0100

    Update on wsaver/ Adding and enabling search optio on left Side

--- Worker.py
+++ Worker.py
@@ -13,4 +13,5 @@ class Worker(QObject):
 
 
     @Slot(str, result="QVariantList")
-    def slot_forecast(self, filename):
+    def slot_forecast(self, filename="forecasts.json"):
+        data = import_forecast(filename)
