34fa86f4d238bce13df58a092839c694db12ec1c
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-28 11:42:25 +0100

    Update on wsaver/ Parse local forecast into UI

--- git-sync.sh
+++ git-sync.sh
@@ -4,7 +4,7 @@ while true
 do
 	echo "_______________"$i"_________________"
 	git add -A
-	git commit -am "Update on wsaver/ Adding and enabling search optio on left Side"
+	git commit -am "Update on wsaver/ Parse local forecast into UI"
 	git push
 	sleep 30
 	echo "Hi, bm7. A update has been done :)"
