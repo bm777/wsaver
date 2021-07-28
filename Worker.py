4bdd0c6c742e4b200af7df03616e8e2d2fd73f72
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-28 02:04:37 +0100

    Update on wsaver/ Adding and enabling search optio on left Side

--- Worker.py
+++ Worker.py
@@ -1,3 +1,10 @@
 # This Python file uses the following encoding: utf-8
 from PySide2 import QtWidgets
 from PySide2.QtCore import QObject, Signal, Slot, Property
+import os
+
+class Worker(QObject):
+    """docstring for Worker."""
+
+    def __init__(self):
+        QObject.__init__(self)
