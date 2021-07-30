3f7c7d279a6d68180e6d006d736fc3122a02e97b
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-30 18:30:40 +0100

    Update on wsaver/ Added Yoruba language

--- git-sync.sh
+++ git-sync.sh
@@ -4,7 +4,7 @@ while true
 do
 	echo "_______________"$i"_________________"
 	git add -A
-	git commit -am "Update on wsaver/ Adding Yoruba language"
+	git commit -am "Update on wsaver/ Added Yoruba language"
 	git push
 	sleep 30
 	echo "Hi, bm7. A update has been done :)"
