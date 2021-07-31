2f9d91633d1f515e5963542c61a5e8273e82b5ca
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-31 17:15:49 +0100

    Update on wsaver/ Preprocesing for the volumetric soil data

--- local_request.py
+++ local_request.py
@@ -41,6 +41,33 @@ def percent_flood(df, data):
     return (final/288) * 100
 
 
+def import_and_merge_wind(file1, file2):
+    # Function to read data from local csv file on the hard drive
+    # Args:
+    # - filename : the file name of the csv which contains
+    #   forecasts data of wind speed data
+    # - the function return merged dataframe based on date column
+    file1 = os.path.join(os.path.abspath(""), file1)
+    file2 = os.path.join(os.path.abspath(""), file2)
+
+    # load csv file on dataframe
+    df1 = pd.read_csv(file1)
+    #df2 = pd.read_csv(file2)
+
+    # split the df
+    df1[["value", "t1", "t2", "t3"]] = df1.value.str.split(" ", expand=True)
+    #df2[["speed", "t1", "t2", "t3"]] = df2.value.str.split(" ", expand=True)
+
+    # remove column unusable
+    df1.pop("t1"), df1.pop("t2"), df1.pop("t3")
+    #df2.pop("value"), df2.pop("t1"), df2.pop("t2"), df2.pop("t3")
+
+    # merge df1 and df2 based on date column
+    #df = pd.merge(df1, df2, on="date")
+
+    return df1
+
+
 if __name__ == '__main__':
     #print(import_forecast("forecast.json"))
     file = "era5_volumetric_soil_water_layer_1-hourly-5.0405866_7.9194225.csv"
