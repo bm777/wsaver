8796a590eb3fc0d4a6ce51c16ec974679e8fb670
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-07-31 08:32:26 +0100

    Update on wsaver/ Preprocesing for the volumetric soil data

--- local_request.py
+++ local_request.py
@@ -35,7 +35,10 @@ def select_12_days(df, result_index):
     current = result_index
     return df.iloc[current-24*12 : current]
 
-def percent_flood(df, data)
+def percent_flood(df, data):
+    list_selection = df.tolist()
+    final = len([x for x in list_selection if data > x])
+    return (final/288) * 100
 
 
 if __name__ == '__main__':
