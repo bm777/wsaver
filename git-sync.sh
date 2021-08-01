283d162aefb853bd6df62d2cdfb0a207fe9f830b
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-08-01 14:28:00 +0100

    Update on wsaver/ Update

--- git-sync.sh
+++ git-sync.sh
@@ -4,7 +4,7 @@ while true
 do
 	echo "_______________"$i"_________________"
 	git add -A
-	git commit -am "Update on wsaver/ Preprocesing for the volumetric soil data"
+	git commit -am "Update on wsaver/ Update"
 	git push
 	sleep 30
 	echo "Hi, bm7. A update has been done :)"
